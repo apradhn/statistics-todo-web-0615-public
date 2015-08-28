6e75ac789c2fef10610d813e42ed3a4d04cbe910
apradhn <abhijit.pradhan6@gmail.com>
2015-08-28 00:25:09 -0400

    completes challenge

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,1 +1,16 @@
-# your code here
+class Array
+  def mean
+    (self.inject(0){|sum, n| sum + n.to_f} / self.size).round(2)
+  end
+
+  def median
+    self.sort[self.size / 2]
+  end
+
+  def mode
+    self.each_with_object({}) do |a, hash| 
+      hash[a] = self.count{|b| a == b}
+    end
+    .max_by{|key, value| value}.first
+  end
+end
